5fa439854e1adf55c06a37c699b3fd922fec03be
sobbuh <sobbuh7@gmail.com>
2017-07-26 10:07:10 -0700

    add router func to search button

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import { Link } from 'react-router-dom'
 
 class BookShelf extends Component{
 
@@ -164,7 +165,7 @@ class BookShelf extends Component{
 		              </div>
 		            </div>
 		            <div className="open-search">
-		              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+		              <Link to="/search">Add a book</Link>
 		            </div>
 				</div>
 				
