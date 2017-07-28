cbfc388e2284b4eb01a02848e1d1631f91e55756
sobbuh <sobbuh7@gmail.com>
2017-07-28 16:52:14 -0700

    refactor book for smaller components

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
+import Book from './Book.js'
 
 
 class BookSearch extends Component{
@@ -41,27 +42,8 @@ class BookSearch extends Component{
       <div className="search-books-results">
         <ol className="books-grid">
           {searchedBooks.map((book) => (  
-                          
-                          <li key={book.id}>
-                            <div className="book">
-                              <div className="book-top">
-                                <div className="book-cover" style={{ width: 128, height: 193, 
-                                  backgroundImage: `url(${book.imageLinks ? book.imageLinks.thumbnail : 'https://books.google.com/googlebooks/images/no_cover_thumb.gif'})` }}></div>                         
-                                <div className="book-shelf-changer">
-                                  <select value={book.shelf} onChange={ (event) => { updateStatus({book: book, shelf: event.target.value})} }>
-                                    <option value="" disabled>Move to...</option>
-                                    <option value="currentlyReading">Currently Reading</option>
-                                    <option value="wantToRead">Want to Read</option>
-                                    <option value="read">Read</option>
-                                    <option value="none">None</option>
-                                  </select>
-                                </div>
-                              </div>
-                              <div className="book-title">{book.title}</div>
-                              <div className="book-authors">{book.authors ? book.authors.join(", ") : "No Author Given"}</div>
-                            </div>
-                          </li>
-                        ))}
+          <Book book={book} updateStatus={updateStatus} />
+          ))}
 
         </ol>
       </div>
--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
+import Book from './Book.js'
 
 const BookShelf = (props) => {
       
@@ -15,24 +16,7 @@ const BookShelf = (props) => {
 	        <div className="bookshelf-books">
 	          <ol className="books-grid">
 	          {books.filter(book => book.shelf === shelfLabel).map(book => (
-	            <li key={book.id}>
-	              <div className="book">
-	                <div className="book-top">
-	                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
-	                  <div className="book-shelf-changer">
-	                    <select value={book.shelf} onChange={ (event) => { updateStatus({book: book, shelf: event.target.value})} }>
-	                      <option value="" disabled>Move to...</option>
-	                      <option value="currentlyReading">Currently Reading</option>
-	                      <option value="wantToRead">Want to Read</option>
-	                      <option value="read">Read</option>
-	                      <option value="none">None</option>
-	                    </select>
-	                  </div>
-	                </div>
-	                <div className="book-title">{book.title}</div>
-	                <div className="book-authors">{book.authors.join(', ')}</div>
-	              </div>
-	            </li>
+	           <Book book={book} updateStatus={updateStatus}/>
 	            ))
 	          }
 	          </ol>
